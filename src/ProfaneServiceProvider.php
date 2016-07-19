a8a3774305064b93c680384d5064115e58d72246
Arandi Lopez <arandilopez.93@gmail.com>
2016-07-18 22:47:57 -0500

    add validation message with replacer

--- src/ProfaneServiceProvider.php
+++ src/ProfaneServiceProvider.php
@@ -3,16 +3,25 @@
 namespace LaravelProfane;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Lang;
 
 class ProfaneServiceProvider extends ServiceProvider
 {
   public function register()
   {
-    # code...
+    $this->loadTranslationsFrom(__DIR__.'/lang', 'laravel-profane');
+
+    $this->publishes([
+        __DIR__.'/lang' => resource_path('lang/vendor/laravel-profane'),
+    ]);
   }
 
   public function boot()
   {
     Validator::extend('profane', 'LaravelProfane\ProfaneValidator@validate');
+
+    Validator::replacer('profane', function($message, $attribute, $rule, $parameters) {
+        return str_replace(':attribute', $attribute, $message);
+    });
   }
 }
