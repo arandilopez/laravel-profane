26a53f0901e64deadce6cedac15c8fc35d4656a8
Riza Saputra <riza.saputra@2359media.com>
2017-10-12 11:32:49 +0700

    add Indonesian dictionary

---
+++ src/dict/id.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * List of common swear words in Indonesian, sorted alphabetically.
+ * Since some common swear words are simply animal names, those animal names are omitted from this list.
+ * 
+ * Provided by @rizasaputra
+ */
+
+return [
+	'anjink',
+	'asu',
+	'bajingan',
+	'banci',
+	'bego',
+	'bencong',
+	'bolot',
+	'brengsek',
+	'budeg',
+	'budek',
+	'cukimai',
+	'eek',
+	'ewe',
+	'gebleg',
+	'geblek',
+	'gelo',
+	'gembel',
+	'gila',
+	'goblog',
+	'goblok',
+	'itil',
+	'jablay',
+	'jancok',
+	'jancuk',
+	'jembut',
+	'jing',
+	'kacrut',
+	'kampret',
+	'keparat',
+	'kontol',
+	'koplak',
+	'koplok',
+	'kunyuk',
+	'lonte',
+	'maho',
+	'memek',
+	'monyong',
+	'ngehe',
+	'ngentot',
+	'njing',
+	'ontohod',
+	'pantek',
+	'pecun',
+	'peju',
+	'perek',
+	'sarap',
+	'sinting',
+	'sompret',
+	'tai',
+	'titit',
+	'tolol',
+	'udik',
+	'upil'
+];
---
+++ src/lang/id/validation.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'profane' => ':attribute mengandung konten yang vulgar',
+];
