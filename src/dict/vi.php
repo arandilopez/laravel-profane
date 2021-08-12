b331148a39bf7d1f915c06383c8e267b71a77958
teobatman00 <teobatman00@gmail.com>
2021-08-12 10:40:14 +0700

    Update vietnamese dictionary profane

---
+++ src/dict/vi.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'con cạc',
+    'đỉ chó',
+    'đỉ mẹ',
+    'má mày',
+    'đủ má',
+    'đỉ lồn',
+    'thằng đỉ',
+    'cc',
+    'chó má',
+    'thằng lồn',
+    'đụ mẹ',
+    'mẹ mày',
+    'đỉ mẹ mày',
+    'cặc lồn',
+    'lồn mẹ',
+    'đủy mẹ',
+    'thằng lờ',
+    'đỉ má',
+    'lồn chó',
+    'nứng lồn',
+    'cặc lồn'
+];
